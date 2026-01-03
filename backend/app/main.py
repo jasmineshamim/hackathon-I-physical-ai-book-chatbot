c5b07d633f52733148e0a229505744ea1e67cceb
jasmine sheikh <169073734+jasmineshamim@users.noreply.github.com>
2026-01-03 13:17:01 +0500

    Change CORS settings to allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,10 +13,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[
-            "https://hackathon-i-physical-ai-book-chatbot-q75dbtepf.vercel.app",  # fixed typo
-            "http://localhost:3000"
-        ],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
